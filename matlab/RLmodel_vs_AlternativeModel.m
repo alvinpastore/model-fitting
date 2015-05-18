f7903094691838ba2872d7b46d64fd72c72b90e1
Alvin <alvin.pastore@gmail.com>
2015-05-18 15:01:17 +0100

    RL comparison script extends to more actions

--- matlab/RLmodel_vs_AlternativeModel.m
+++ matlab/RLmodel_vs_AlternativeModel.m
@@ -1,20 +1,37 @@
 % Setting for the comparison 
 
-% 4 compare against alternative model
+% 4 compare against alternative model (including nogamma)
 % 5 compare against random model
 CONFIG_COMPARISON = 4;
 
-% 0 for alternative models
-% 1 for nogamma models (less columns in results)
+% 0 for other models
+% 1 for nogamma models (less columns in results (5 instead of 6))
 NOGAMMA_COMPARISON = 0;
 
-MODEL_DESCRIPTION = 'Dumb Player 3';
+% number of actions for RANDOM COMPARISON
+ACTIONS_AMOUNT = 5;
+
+% name of ALTERNATIVE MODEL
+ALTERNATIVE_MODEL = 'Dumb Player 5';
+
+MODEL_DESCRIPTION = [num2str(ACTIONS_AMOUNT),' actions vs '];
+
+if CONFIG_COMPARISON == 5
+    MODEL_DESCRIPTION = [MODEL_DESCRIPTION, 'Random Model'];
+else
+    if NOGAMMA_COMPARISON == 1
+        MODEL_DESCRIPTION = [MODEL_DESCRIPTION, 'NoGamma Model'];
+    else
+        MODEL_DESCRIPTION = [MODEL_DESCRIPTION, ALTERNATIVE_MODEL];
+    end
+end
+
 % get list of players from model results
 players = unique(res_model(:,1));
 playersAmount = size(players,1);
 
 % store random model precision
-p_random = 1/3;
+p_random = 1/ACTIONS_AMOUNT;
 
 performances = zeros(playersAmount,5);
 outperformers = [];
@@ -27,14 +44,19 @@ for playerID = 0:playersAmount-1
     % search for player best alternative model precision
     p_alternative_lines = find(res_alternative(:,1) == playerID); 
     p_alternative_best  = max(res_alternative(p_alternative_lines,(6-NOGAMMA_COMPARISON)));
-    % calculate improvement rates (over alternative model and over random model)
+    
+    
     if p_alternative_best == 0
         disp(playerID)
         outperformers(size(outperformers,2)+1) = playerID;
     else
+        % calculate improvement rates (over alternative model)
         improv_rate_altern = ((p_model_best-p_alternative_best)/p_alternative_best)*100;
-        improv_rate_random = ((p_model_best-p_random)/p_random)*100;
     end
+    
+    % calculate improvement rates (over random model)
+    improv_rate_random = ((p_model_best-p_random)/p_random)*100;
+    
     % populate performances matrix
     performances(playerID+1,:) = [playerID p_model_best p_alternative_best improv_rate_altern improv_rate_random];
     
@@ -42,28 +64,33 @@ end
 
 % calculate avg improvement performance
 avg_improvement = mean(performances(:,CONFIG_COMPARISON));
-tit = ['Precision Improvement, Avg: ',num2str(avg_improvement), ' outperformers: ',mat2str(outperformers)];
+tit = [num2str(ACTIONS_AMOUNT),' Actions - Precision Improvement, Avg: ',num2str(avg_improvement)];
+if size(outperformers,2) > 0
+    tit = [tit , ' outperformers: ',mat2str(outperformers)];
+end
 
-% print unsorted bar chart
-figure(1);
-hold on
-bar(performances(:,1),performances(:,CONFIG_COMPARISON));
-plot([-1 46],[avg_improvement avg_improvement],'-r','LineWidth',2);
 
-title(tit,'FontSize', 24);
-xlabel('Players')
-ylabel(['Improvement over ',MODEL_DESCRIPTION])
-axis([-1 46 min(performances(:,CONFIG_COMPARISON)) max(performances(:,CONFIG_COMPARISON))]);
-set(gca,'fontsize', 24)
-legend('Individual improvement','Avg Improvement');
-hold off
+%%% PRINT unsorted bar chart
+%figure(1);
+%hold on
+%bar(performances(:,1),performances(:,CONFIG_COMPARISON));
+%plot([-1 46],[avg_improvement avg_improvement],'-r','LineWidth',2);
+
+%title(tit,'FontSize', 24);
+%xlabel('Players')
+%ylabel(['Improvement over ',MODEL_DESCRIPTION])
+%axis([-1 46 min(performances(:,CONFIG_COMPARISON)) max(performances(:,CONFIG_COMPARISON))]);
+%set(gca,'fontsize', 24)
+%legend('Individual improvement','Avg Improvement');
+%hold off
+
 %fileName = 'graphs_improvements\g_RL_RandBins_unsorted.png';
 %print(fig,'-dpng',fileName)
     
 % sort according to the improvement 
 sorted_performances = sortrows(performances,CONFIG_COMPARISON);
 
-% print sorted bar chart
+% PRINT sorted bar chart
 figure(2);
 hold on
 bar(performances(:,1),sorted_performances(:,CONFIG_COMPARISON));
