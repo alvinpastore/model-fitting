6383ca62f2e0caefdd29ef90e9f75050ecf8b447
Alvin <alvin.pastore@gmail.com>
2015-09-04 12:18:02 +0100

    matlab side code figures and variance

---
+++ matlab/writeFig300ppi.m
@@ -0,0 +1,17 @@
+
+        %% Print an image in 300ppi with white background
+
+function writeFig300ppi(figNo, fileName)
+
+figureNameTif = [fileName, char('.tif')];
+
+%make the backgroung white
+set(figNo,'color','w');
+
+%get a frame of the image
+f=getframe(figNo);
+
+colormap(f.colormap);
+
+%print image with 300ppi resolution
+imwrite(f.cdata, figureNameTif, 'Resolution', 300); 
