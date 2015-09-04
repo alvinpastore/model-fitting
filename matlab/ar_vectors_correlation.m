a3c532e210eacb5ab42bde79537bacff229d0ce5
Alvin <alvin.pastore@gmail.com>
2015-09-04 12:00:19 +0100

    matlab folder Actions - Reward correlation

--- matlab/ar_vectors_correlation.m
+++ matlab/ar_vectors_correlation.m
@@ -1,26 +1,56 @@
+% BOOLEAN flag for discretised correlations
+DISCRETISE = 1;
+
+%risk column is normally 2 but it is 4 for the discretised riskiness
+RISK_COLUMN = 2 + 2*DISCRETISE; 
+
+% thresholds for N actions (manually checked from data/uniform_N.txt)
+thresholds = [0.066 0.166];
+
+%% Discretise riskiness  
+% TODO generalise code for N actions 
+if DISCRETISE == 1
+    t1 = 1 .* (arvectors(:,2) < thresholds(1));
+    t2 = 2 .* (arvectors(:,2) > thresholds(1)) .* (arvectors(:,2) < thresholds(2));
+    t3 = 3 .* (arvectors(:,2) > thresholds(2));
+    
+    arvectors(:,4) = t1 + t2 + t3; % discretised riskiness on column 4
+end
+
+% retrieve players 
 players = unique(arvectors(:,1));
 playersAmount = size(players,1);
 
+% instantiate correlation vectors
 correlations = zeros(playersAmount,3);
 
-for playerID = 0:playersAmount-1
-    %disp(playerID);
+
+for playerID = 0:playersAmount-1    
+    close all;
+    % retrieve player lines
     player_lines = find(arvectors(:,1) == playerID); 
-    risk_vector = arvectors(player_lines,2);
+    
+    % identify risk and reward vectors
+    risk_vector = arvectors(player_lines,RISK_COLUMN);
     reward_vector = arvectors(player_lines,3);
+    
+    % calculate correlation and p-value
     [r,p] = corrcoef(risk_vector , reward_vector);
     R = r(1,2);
     P = p(1,2);
     
+    % populate correlations vector
     correlations(playerID+1,:)  = [playerID R P];
     
-    % interesting p-values
+    % interesting p-values (<1 for all)
     if P < 1
         disp(playerID);
-        % sort vectors according to riskiness, keep association across rewards
+        
+        % calculate linear regression 
         p = polyfit(risk_vector,reward_vector,1);
         pp = polyval(p,risk_vector); 
         
+        % generate figure
         figure();
         hold on
         
@@ -31,10 +61,15 @@ for playerID = 0:playersAmount-1
         xlabel('Risk');
         ylabel('Reward');
         legend('Scatter','Regression','Location','NorthWest')
-        hold off
         
-        fileName = ['graphs_correlation_risk_reward\g-',num2str(playerID),'.png'];
+        fileName = ['graphs_correlation_risk_reward\g-',num2str(playerID)];
+        
+        if DISCRETISE == 1
+            fileName = [fileName, 'discretised.png'];
+        end
+        
         print(gcf, '-dpng', fileName)
+        hold off
     end
     
 end
