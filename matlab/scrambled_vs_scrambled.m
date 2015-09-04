8b63bb8f6f7dfd6f935a1ebdfbf0075e7ab24d12
Alvin <alvin.pastore@gmail.com>
2015-09-04 12:16:34 +0100

    scrambled vs scrambled code

---
+++ matlab/scrambled_vs_scrambled.m
@@ -0,0 +1,144 @@
+
+MLE_iter = [0, 1, 2, 3, 4, 5, 6, 7 ,8 ,9];
+
+
+% load model results
+current_results = 'res3';
+model = eval(current_results);
+model = model(find(model(:,2) ~= 0),:);
+
+SCRAMBLEDMLE_NAME = 'MLESCRAM';
+
+% count players
+players = unique(model(:,1));
+playersAmount = size(players,1);
+
+% offset = amount of models in gridsearch
+% = 5alphas X 4betas X 5gammas
+OFFSET = 100;
+
+% cell array to hold info about all comparisons
+scram_compare = cell(length(MLE_iter)*length(MLE_iter),1);
+pv_idx = 1;
+
+% permutations of models minus model with itself (N*N)-N
+PERMUTATIONS = (length(MLE_iter)*length(MLE_iter))-length(MLE_iter);
+% save the players who are better than scrambled
+better_than_scrambled = zeros(playersAmount+1,PERMUTATIONS);
+bts_idx = 1;
+
+for i = MLE_iter
+    
+    % scram model first
+    first_scrambled_MLE = eval([SCRAMBLEDMLE_NAME,num2str(i)]);
+    avg_first_scrambled = mean(first_scrambled_MLE(:,5:end).').';
+    
+    for k = MLE_iter
+        if i ~= k
+            disp(['i ',num2str(i),' k: ',num2str(k),' bts_idx: ',num2str(bts_idx)]);
+            % initialise performances structure
+            % [playerID first_best_MLE second_best_MLE first_best_MLE < second_best_MLE pv_MLE] 
+            current_pvalues = zeros(playersAmount,5);
+            
+            % scram model second
+            second_scrambled_MLE = eval([SCRAMBLEDMLE_NAME,num2str(k)]);
+            avg_second_scrambled = mean(second_scrambled_MLE(:,5:end).').';
+            
+            for playerID = 0:playersAmount-1
+
+                % find player lines
+                player_lines_MLE = find(first_scrambled_MLE(:,1) == playerID);
+                
+                % find best MLE first scrambled model
+                [first_best_MLE, first_best_MLE_line] = min(avg_first_scrambled(player_lines_MLE,:));
+               
+                corresponding_best_MLE_line = first_best_MLE_line + (playerID * OFFSET);
+                first_MLE_line = first_scrambled_MLE(corresponding_best_MLE_line,5:end);
+
+                % find best MLE second scrambled model
+                [second_best_MLE, second_best_MLE_line] = min(avg_second_scrambled(player_lines_MLE,:));
+               
+                corresponding_best_MLE_line = second_best_MLE_line + (playerID * OFFSET);
+                second_MLE_line = second_scrambled_MLE(corresponding_best_MLE_line,5:end);
+                
+                % calculate p-value 
+                [pv_MLE] = ranksum(first_MLE_line,second_MLE_line);
+
+                current_pvalues(playerID+1,:) = [playerID first_best_MLE second_best_MLE first_best_MLE < second_best_MLE pv_MLE];
+
+            end
+
+            % add column of 1 for players better than best scrambled with statistical significance
+            better_than_scrambled(1:end-1,bts_idx) = current_pvalues(:,4) & current_pvalues(:,5) < 0.01;
+            bts_idx = bts_idx + 1;
+            scram_compare{pv_idx} = current_pvalues;
+            pv_idx = pv_idx + 1;
+        end
+    end
+end
+
+better_than_scrambled(end,:) = sum(better_than_scrambled,1);
+
+disp(better_than_scrambled);
+avg_model = 0.55;
+avg_players = sum(better_than_scrambled(47,:))/length(better_than_scrambled);
+disp(['average players scram_vs_scram ',num2str(avg_players)]);
+avg_percent = avg_players/length(players);
+disp(['average percentage ',num2str(avg_percent)]);
+zeta = 1.96;%2.57;
+
+width = zeta*sqrt( (avg_percent * (1 - avg_percent)) / (length(players) * length(better_than_scrambled) ));
+disp(['width scrambled',num2str(width)]);
+disp(['min ',num2str(avg_percent-width),' max ',num2str(avg_percent+width)]);
+
+hold on;
+errorbar(1,avg_percent,width,width,'bx');
+plot(1,avg_percent,'xb','MarkerSize',10);
+
+width = zeta*sqrt( (avg_model * (1 - avg_model)) / (length(players) * length(MLE_iter) ));
+disp(['width model',num2str(width)]);
+errorbar(1,avg_model, width,width,'rx');
+plot(1,avg_model,'xr','MarkerSize',10);
+
+axis([0.9 1.1 avg_percent - 2 * width max(avg_model + 2 * width,avg_percent + 2 * width)]);
+title('Ranked vs Scrambled bins (95% confidence)','FontSize',20);
+ylabel('% players better fitted by ranked','FontSize',20);
+
+
+%% AIC delta test
+
+NUM_PARAM = 3;
+scram_DELTA = zeros(length(players),length(MLE_iter));
+
+for playerID = 0:playersAmount-1
+    % find best model MLE and calculate AIC
+
+    % find player lines in model and MLE results 
+    % (second condition to avoid random models where beta = 0)
+    player_lines_model = find(model(:,1) == playerID  ); 
+
+    % find best MLE full model 
+    [p_best_MLE, p_best_MLE_line] = min(model(player_lines_model,5));
+    aic_model = aicbic(p_best_MLE,NUM_PARAM);
+    
+    % find best scrambled MLE and calculate AIC
+    for i = MLE_iter
+        
+        % load i-th scrambled and calculate averages of MLEs
+        scrambled_MLE = eval([SCRAMBLEDMLE_NAME,num2str(i)]);
+        avg_scrambled = mean(scrambled_MLE(:,5:end).').';
+        
+        % find player lines
+        player_lines_MLE = find(scrambled_MLE(:,1) == playerID);
+        
+        % find best MLE in i-th scrambled model
+        [s_best_MLE, best_MLE_line] = min(avg_scrambled(player_lines_MLE,:));
+        
+        % calculate AIC for best MLE for i-th scrambled 
+        aic_scram = aicbic(s_best_MLE,NUM_PARAM);
+        
+        % calculate difference and store AIC-DELTA
+        scram_DELTA(playerID + 1,i + 1) = aic_model-aic_scram;
+        
+    end
+end
