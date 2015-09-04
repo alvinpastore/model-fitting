76a47d91ea9fab866c163306e285a845c7a1867f
Alvin <alvin.pastore@gmail.com>
2015-09-04 12:26:39 +0100

    call model fit routine generalised to number of states variable

--- python/code/call_modelFitForward.py
+++ python/code/call_modelFitForward.py
@@ -3,14 +3,15 @@ import sys
 import os
 
 """ CALLS modelFitForward N times with the random bins"""
-""" python call_modelFitForward nIterations CAP nActions randomBinsAmount"""
+""" python call_modelFitForward nIterations CAP nActions nStates randomBinsAmount"""
 
 nIterations = sys.argv[1]
 CAP = sys.argv[2]
 nActions = sys.argv[3]
-randomBinsAmount = int(sys.argv[4])  # number of scrambled bins files available
+nStates = sys.argv[4]
+randomBinsAmount = int(sys.argv[5])  # number of scrambled bins files available
 path = os.path.dirname(os.path.realpath(__file__))
 
-sp = [subprocess.Popen("python " + path + "/modelFitForward.py " + nIterations + " "
-                       + CAP + " ur" + str(_) + " " + nActions, shell=True) for _ in xrange(randomBinsAmount)]
+sp = [subprocess.Popen("python " + path + "/modelFitForward.py " + nIterations + " " + CAP + " ur" + str(_) +
+                       " " + nActions + " " + nStates, shell=True) for _ in xrange(randomBinsAmount)]
 
