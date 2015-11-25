87bf0cc141f2cc210b4c56083f2dc04e3de75516
Alvin <alvin.pastore@gmail.com>
2015-11-25 12:04:04 +0000

    reward histogram htan changes for presentation graph

--- matlab/reward_histogram.m
+++ matlab/reward_histogram.m
@@ -28,22 +28,23 @@
 %hold off;
 
 close all;
-rews = playerstats25cap3act;
+rews = csvread('results/stats/rewards25_cap.csv');
+%playerstats25cap3act;
 
 
-x = min(rews(:,1)):1000:max(rews(:,1));
-
+%x = min(rews(:,1)):100:max(rews(:,1));
+x = (min(rews(:,1))-max(rews(:,1))/10):100:(max(rews(:,1))+max(rews(:,1))/10);
 sigma = 500;
-y = (1-exp(-x./sigma))./(1+exp(-x./sigma));
-yy = (1-exp(-x./10000))./(1+exp(-x./10000));
+y1 = (1-exp(-x./sigma))./(1+exp(-x./sigma));
+y2 = (1-exp(-x./10000))./(1+exp(-x./10000));
 
 %zz =(1-exp(-u/1000))/(1+exp(-u/1000));
 
 fig = figure(1);
 hold on;
-plot(x,y,'-r');
-scatter(rews(:,1),rews(:,2), 'xb' );
-%plot(x,yy,'-g');
+plot(x,y1,'-r', 'LineWidth',7);
+scatter(rews(:,1),rews(:,2),150,'filled' ,'b');
+%plot(x,y2,'-g');
 %set(a,'color','r');
 %set(a,'color','g');
 
@@ -51,13 +52,13 @@ scatter(rews(:,1),rews(:,2), 'xb' );
 %scatter(x,y,'xb');
 hold on;
 %colormap('Jet');
-title('Reward distribution');
-xlabel('htan sigma = 500')
-ylabel('rewards')
+%title('Reward distribution');
+%xlabel('htan sigma = 500')
+%ylabel('rewards')
 
 %legend('Hyperbolic tangent','Reward','Location','NorthEast')
 
-axis([min(rews(:,1)) max(rews(:,1)) -1.1 1.1])
+axis([min(rews(:,1))-max(rews(:,1))/10 max(rews(:,1))+max(rews(:,1))/10 -1.1 1.1])
 
 hold off;
 
