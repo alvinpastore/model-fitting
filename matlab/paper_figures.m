fafd9b83b3895566c447b4f3ee5e4f945c72e780
Alvin <alvin.pastore@gmail.com>
2016-01-13 12:30:59 +0000

    Paper/presentation figures
    1) Now can generate the figures for paper or presentation (if no saved
    figures are needed use 0 for both PAPER and PRESENTATION)
    2) fit has been renamed to performance_fit as fit is a function in
    matlab
    3) figures are generated both in full and split in to halves

--- matlab/paper_figures.m
+++ matlab/paper_figures.m
@@ -1,6 +1,12 @@
 
 tic;
 
+PAPER = 1;
+PRESENTATION = 0;
+size_X = 20;
+size_Y = 15;
+FONT_SIZE = 20;
+ASTERISK_OFFSET = 23;
 % res3 are the results for the fullmodel
 fullmodel = csvread('results/after_money_1k/_fullModel_2states_profit/Negative_25cap_3act_1000rep_0.1-1.0_alpha10.0-40.0_beta0.01-0.999_gamma_u.csv');
 model = fullmodel(find(fullmodel(:,2) ~= 0),:);
@@ -17,8 +23,10 @@ players = unique(model(:,1));
 playersAmount = size(players,1);
 
 % data structure to hold the performances of all the results available
-fit = zeros(playersAmount,8);
+performance_fit = zeros(playersAmount,8);
 
+% column which contains MLE in the results
+MLE_column = 5;
 
 % for every player calculate best model and compare with random and nogamma
 for playerID = 0:playersAmount-1
@@ -26,154 +34,216 @@ for playerID = 0:playersAmount-1
     % search for player random MLE
     random_MLE = fullmodel(find(fullmodel(:,1) == playerID & fullmodel(:,2) == 0),5); 
         
-    % search for player best model precision
+    % search for player best model MLE
     model_lines = find(model(:,1) == playerID); 
-    model_MLE  = min(model(model_lines,5));
+    model_MLE  = min(model(model_lines,MLE_column));
 
-    % search for player best nogamma precision
+    % search for player best nogamma MLE
     nogamma_lines = find(nogamma(:,1) == playerID); 
-    nogamma_MLE  = min(nogamma(nogamma_lines,5));
+    nogamma_MLE  = min(nogamma(nogamma_lines,MLE_column));
     
     % calculate p-value (likelihood ratio)
-    % calculate Likelihood ratio (best precision model)
+    % calculate Likelihood ratio (best MLE model)
     chi_value_random = 2 * (random_MLE - model_MLE);
     p_rand_MLE = 1-chi2cdf(chi_value_random,dof);
     
     % calculate p-value (likelihood ratio)
-    % calculate Likelihood ratio (best precision model)
+    % calculate Likelihood ratio (best MLE model)
     chi_value_nogamma = 2 * (nogamma_MLE - model_MLE);
     p_nog_MLE = 1-chi2cdf(chi_value_nogamma,dof);
 
     % populate performances matrix
-    fit(playerID+1,:) = [playerID model_MLE random_MLE nogamma_MLE p_nog_MLE p_rand_MLE chi_value_random chi_value_nogamma];
+    performance_fit(playerID+1,:) = [playerID model_MLE random_MLE nogamma_MLE p_nog_MLE p_rand_MLE chi_value_random chi_value_nogamma];
 
 end
 close all;
-fit = sortrows(fit,-3);
-path = 'graphs/paper/new/';
-size_X = 20;
-size_Y = 15;
+performance_fit = sortrows(performance_fit,-3);
+
+if PAPER
+    path = 'graphs/paper/new/';
+elseif PRESENTATION
+    path = 'graphs/presentation/';
+end
+
 %%%%%%%%% RANDOM PLOTs %%%%%%%%%%%%
+
+%% RANDOM MLE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% full image
+rand0_comparison = figure();
+hold on;
+bar(performance_fit(:,2),'FaceColor',[0.7,0.7,0.7]);
+plot(1:1:length(performance_fit),performance_fit(1:length(performance_fit),3),'-.r','LineWidth',4);
+hold off;
+xlabel('Players','FontSize',FONT_SIZE);
+ylabel('MLE','FontSize',FONT_SIZE);
+axis([0 47 0 30])
+set(gca,'FontSize',FONT_SIZE);
+
+if PRESENTATION || PAPER
+    set(gcf, 'PaperUnits', 'centimeters');
+    set(gcf, 'PaperPosition', [0 0 size_X size_Y]);
+    saveas(gcf,[path,'vsRandom_full.eps']);
+    print(gcf, '-depsc2', '-loose', [path,'vsRandom_full_loose']);
+end
 
 %% RANDOM MLE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% first half
 rand1_comparison = figure();
 hold on;
-bar(fit(1:23,2),'FaceColor',[0.7,0.7,0.7]);
-plot(1:1:23,fit(1:23,3),'--k');
+bar(performance_fit(1:23,2),'FaceColor',[0.7,0.7,0.7]);
+plot(1:1:23,performance_fit(1:23,3),'-.r','LineWidth',4);
 hold off;
-xlabel('Players','FontSize',20);
-ylabel('MLE','FontSize',20);
+xlabel('Players','FontSize',FONT_SIZE);
+ylabel('MLE','FontSize',FONT_SIZE);
 axis([0 24 0 30])
-set(gca,'FontSize',20);
+set(gca,'FontSize',FONT_SIZE);
 set(gca,'XTick',[1,5,10,15,20,25,30,35,40,45],...  
         'XTickLabel',[1,5,10,15,20,25,30,35,40,45]);
     
-set(gcf, 'PaperUnits', 'centimeters');
-set(gcf, 'PaperPosition', [0 0 size_X size_Y]);
-saveas(gcf,[path,'vsRandom1.eps']);
-print(gcf, '-depsc2', '-loose', [path,'vsRandom1_loose']);
+if PRESENTATION || PAPER
+    set(gcf, 'PaperUnits', 'centimeters');
+    set(gcf, 'PaperPosition', [0 0 size_X size_Y]);
+    saveas(gcf,[path,'vsRandom_half1.eps']);
+    print(gcf, '-depsc2', '-loose', [path,'vsRandom_half1_loose']);
+end
+
 %% RANDOM MLE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% second half
 rand2_comparison = figure();
 hold on;
-bar(24:1:length(fit),fit(24:length(fit),2),'FaceColor',[0.7,0.7,0.7]);
-plot(24:1:length(fit),fit(24:length(fit),3),'--k');
+bar(24:1:length(performance_fit),performance_fit(24:length(performance_fit),2),'FaceColor',[0.7,0.7,0.7]);
+plot(24:1:length(performance_fit),performance_fit(24:length(performance_fit),3),'-.r','LineWidth',4);
 hold off;
-xlabel('Players','FontSize',20);
-ylabel('MLE','FontSize',20);
-axis([23 length(fit)+1 0 30])
-set(gca,'FontSize',20);
-set(gcf, 'PaperUnits', 'centimeters');
-set(gcf, 'PaperPosition', [0 0 size_X size_Y]);
-saveas(gcf,[path,'vsRandom2.eps'])
-print(gcf, '-depsc2', '-loose', [path,'vsRandom2_loose']);
+xlabel('Players','FontSize',FONT_SIZE);
+ylabel('MLE','FontSize',FONT_SIZE);
+axis([23 length(performance_fit)+1 0 30])
+set(gca,'FontSize',FONT_SIZE);
+
+if PRESENTATION || PAPER
+    set(gcf, 'PaperUnits', 'centimeters');
+    set(gcf, 'PaperPosition', [0 0 size_X size_Y]);
+    saveas(gcf,[path,'vsRandom_half2.eps'])
+    print(gcf, '-depsc2', '-loose', [path,'vsRandom_half2_loose']);
+end
+
 %%%%%%%%%% RANDOM Likelihood - RATIO %%%%%%%%%%
 % 
 % rand2_comparison = figure();
 % hold on;
-% bar(fit(:,7),'FaceColor',[0.7,0.7,0.7]);
-% %plot(1:1:length(fit),fit(:,3),'xk');
+% bar(performance_fit(:,7),'FaceColor',[0.7,0.7,0.7]);
+% %plot(1:1:length(performance_fit),performance_fit(:,3),'xk');
 % hold off;
-% xlabel('Players','FontSize',20);
-% ylabel('Likelihood ratio (chi-squared)','FontSize',20);
+% xlabel('Players','FontSize',FONT_SIZE);
+% ylabel('Likelihood ratio (chi-squared)','FontSize',FONT_SIZE);
 % %axis([0 47 0 30])
-% set(gca,'FontSize',20);
+% set(gca,'FontSize',FONT_SIZE);
 % set(gcf, 'PaperUnits', 'centimeters');
 % set(gcf, 'PaperPosition', [0 0 35 25]); %x_width=10cm y_width=15cm
 % saveas(gcf,['graphs/paper/vsRandom1.eps']);
 
 %% %%%%%%%%% %%%%%%%%% NoGamma %%%%%%%%% %%%%%%%%%%%%
-asterisk_offset = 16.7;
-significative = fit(:,5) < 0.05;
+performance_fit = sortrows(performance_fit,-4);
+
+significative = performance_fit(:,5) < 0.05;
 sig1 = significative(1:23);
 sig2 = significative(24:end);
 
 signif_x_1 = find(sig1 > 0);
-signif_y_1 = sig1(signif_x_1)*asterisk_offset; 
+signif_y_1 = sig1(signif_x_1) * ASTERISK_OFFSET; 
 
 signif_x_2 = find(sig2 > 0)+23; % offset for second half of players
-signif_y_2 = sig2(signif_x_2-23)*asterisk_offset; 
-
+signif_y_2 = sig2(signif_x_2-23) * ASTERISK_OFFSET; 
 
 % nogamma bars, diamonds and stars
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % first half
-nogamma1_comparison = figure();
+% % % % % % % % % % % % % % % % % % % % % % % full 
+nogamma0_comparison = figure();
 hold on;
 
-bar(fit(1:23,2),'FaceColor',[0.7,0.7,0.7]);
+bar(performance_fit(:,2),'FaceColor',[0.7,0.7,0.7]);
 
-scatter( 1:1:23 , fit(1:23,4), ...
-100 , [0.2,0.2,0.2],...
-'filled','d','MarkerEdgeColor',[0,0,0],...
-'LineWidth',.5); 
+plot( 1:1:46 , performance_fit(:,4), ...
+'-.g','MarkerEdgeColor',[0,0,0],...
+'LineWidth',4); 
 
 plot(signif_x_1 , signif_y_1,'k*','MarkerSize',15);
 
-axis([0 24 0 18]);
 hold off;
-xlabel('Players','FontSize',20);
-ylabel('MLE','FontSize',20);
-set(gca,'FontSize',20);
-set(gca,'XTick',[1,5,10,15,20,25,30,35,40,45],'YTick',0:5:15,...  
-        'YTickLabel',[0,5,10,15],...
-        'XTickLabel',[1,5,10,15,20,25,30,35,40,45]);
-    
-set(gcf, 'PaperUnits', 'centimeters');
-set(gcf, 'PaperPosition', [0 0 size_X size_Y]); 
-saveas(gcf,[path,'vsNogammaAlt1.eps']);
-print(gcf, '-depsc2', '-loose', [path,'vsNogammaAlt1_loose']);
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % second half
+
+xlabel('Players','FontSize',FONT_SIZE);
+ylabel('MLE','FontSize',FONT_SIZE);
+axis([0 47 0 25]);
+
+set(gca,'FontSize',FONT_SIZE);
+set(gca,'XTick',[1,5,10,15,20,25,30,35,40,45],'YTick',0:5:25,...  
+        'YTickLabel',[0,5,10,15,20,25],...
+        'XTickLabel',[]);
+
+if PRESENTATION || PAPER
+    set(gcf, 'PaperUnits', 'centimeters');
+    set(gcf, 'PaperPosition', [0 0 size_X size_Y]); 
+    saveas(gcf,[path,'vsNogammaAlt1.eps']);
+    print(gcf, '-depsc2', '-loose', [path,'vsNogammaAlt1_loose']);
+end
+% nogamma bars, diamonds and stars
+% % % % % % % % % % % % % % % % % % % % % % % % % first half
+nogamma1_comparison = figure();
+hold on;
+
+bar(performance_fit(1:23,2),'FaceColor',[0.7,0.7,0.7]);
+
+plot( 1:1:23 , performance_fit(1:23,4), ...
+'-.g','MarkerEdgeColor',[0,0,0],...
+'LineWidth',4); 
+
+plot(signif_x_1 , signif_y_1,'k*','MarkerSize',15);
+
+hold off;
+
+xlabel('Players','FontSize',FONT_SIZE);
+ylabel('MLE','FontSize',FONT_SIZE);
+axis([0 24 0 25]);
+
+set(gca,'FontSize',FONT_SIZE);
+set(gca,'XTick',[1,5,10,15,20,25,30,35,40,45],'YTick',0:5:25,...  
+        'YTickLabel',[0,5,10,15,20,25],...
+        'XTickLabel',[]);
+if PRESENTATION || PAPER
+    set(gcf, 'PaperUnits', 'centimeters');
+    set(gcf, 'PaperPosition', [0 0 size_X size_Y]); 
+    saveas(gcf,[path,'vsNogammaAlt1.eps']);
+    print(gcf, '-depsc2', '-loose', [path,'vsNogammaAlt1_loose']);
+end
+% % % % % % % % % % % % % % % % % % % % % % % % % % second half
 nogamma2_comparison = figure();
 hold on;
 
-bar(24:1:length(fit),fit(24:end,2),'FaceColor',[0.7,0.7,0.7]);
+bar(24:1:length(performance_fit),performance_fit(24:end,2),'FaceColor',[0.7,0.7,0.7]);
 
-scatter( 24:1:length(fit) , fit(24:end,4), ...
-100 , [0.2,0.2,0.2],...
-'filled','d','MarkerEdgeColor',[0,0,0],...
-'LineWidth',.5);  
+plot( 24:1:length(performance_fit) , performance_fit(24:end,4), ...
+'-.g','MarkerEdgeColor',[0,0,0],...
+'LineWidth',4);  
 
 plot(signif_x_2 , signif_y_2,'k*','MarkerSize',15);
 
-axis([23 length(fit)+1 0 18]);
 hold off;
-xlabel('Players','FontSize',20);
-ylabel('MLE','FontSize',20);
-set(gca,'FontSize',20);
-set(gca,'XTick',0:5:46,'YTick',0:5:15,...  
-        'YTickLabel',[0,5,10,15],...
-        'XTickLabel',[1,5,10,15,20,25,30,35,40,45]);
-    
-set(gcf, 'PaperUnits', 'centimeters');
-set(gcf, 'PaperPosition', [0 0 size_X size_Y]); 
-saveas(gcf,[path,'vsNogammaAlt2.eps']);
-print(gcf, '-depsc2', '-loose', [path,'vsNogammaAlt2_loose']);
 
+xlabel('Players','FontSize',FONT_SIZE);
+ylabel('MLE','FontSize',FONT_SIZE);
+axis([23 length(performance_fit)+1 0 25]);
+
+set(gca,'FontSize',FONT_SIZE);
+set(gca,'XTick',0:5:46,'YTick',0:5:25,...  
+        'YTickLabel',[0,5,10,15,20,25],...
+        'XTickLabel',[]);
+if PRESENTATION || PAPER
+    set(gcf, 'PaperUnits', 'centimeters');
+    set(gcf, 'PaperPosition', [0 0 size_X size_Y]); 
+    saveas(gcf,[path,'vsNogammaAlt2.eps']);
+    print(gcf, '-depsc2', '-loose', [path,'vsNogammaAlt2_loose']);
+end
 %% nogamma 3d
 % 
 % nogamma3_comparison = figure();
 % handle = gca(1);
 % 
-% bars = [fit(:,[2,4]), significative*20];
+% bars = [performance_fit(:,[2,4]), significative*20];
 % bar_handle = bar3(bars);
 % 
 % alpha(0.4);
@@ -199,8 +269,8 @@ print(gcf, '-depsc2', '-loose', [path,'vsNogammaAlt2_loose']);
 % %set(gca,'XTick',[]);
 % 
 % 
-% ylab = ylabel('Players','FontSize',20);
-% zlab = zlabel('MLE','FontSize',20);
+% ylab = ylabel('Players','FontSize',FONT_SIZE);
+% zlab = zlabel('MLE','FontSize',FONT_SIZE);
 % 
 % %l{1}=' 3 Actions'; l{2}=' 4 Actions'; l{3}=' 5 Actions'; l{4}=' 10 Actions'; 
 % %hLegend = legend(bar_handle,l,'Location',[0.7 0.5 0 0]);
@@ -212,15 +282,15 @@ print(gcf, '-depsc2', '-loose', [path,'vsNogammaAlt2_loose']);
 
 %% no gamma grouped
 % nogamma1_comparison = figure();
-% bar([fit(1:23,2),fit(1:23,4)]);
-% set(gca,'FontSize',20);
+% bar([performance_fit(1:23,2),performance_fit(1:23,4)]);
+% set(gca,'FontSize',FONT_SIZE);
 % set(gcf, 'PaperUnits', 'centimeters');
 % set(gcf, 'PaperPosition', [0 0 35 25]); %x_width=10cm y_width=15cm
 % saveas(gcf,['graphs/paper/vsNogamma1.png']);
 % 
 % nogamma2_comparison = figure();
-% bar([fit(24:end,2),fit(24:end,4)]);
-% set(gca,'FontSize',20);
+% bar([performance_fit(24:end,2),performance_fit(24:end,4)]);
+% set(gca,'FontSize',FONT_SIZE);
 % set(gcf, 'PaperUnits', 'centimeters');
 % set(gcf, 'PaperPosition', [0 0 35 25]); %x_width=10cm y_width=15cm
 % saveas(gcf,['graphs/paper/vsNogamma2.png']);
