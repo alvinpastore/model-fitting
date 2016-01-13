0afe9e65cb747e264bfb40b15ae18b935dbc625d
Alvin <alvin.pastore@gmail.com>
2016-01-13 16:12:31 +0000

    minor changes
    alpha -> alpha_confidence
    MLE_comparison matrix is now a vector
    TODO make it one scrambled at a time to reduce memory consumption

--- matlab/statistical_test_scrambled_2_stage_binomial.m
+++ matlab/statistical_test_scrambled_2_stage_binomial.m
@@ -1,6 +1,7 @@
 % routine for testing scrambled MLEs against RL model
 tic
 
+% TODO rewrite so that scrams are loaded one at a time to reduce memory consumption
 
 % if the MLESCRAMS have been already imported use 0 as input and use a dummy for MLESCRAMS
 % import scrambled MLE matrices, model MLE matrix and resuls matrix
@@ -9,11 +10,12 @@ MLE_iter = 1:SCRAM_NUMBER;
 
 % load model results
 model = res3;
+% remove random models
 model = model(find(model(:,2) ~= 0),:);
 
 
 % offset = amount of models in gridsearch
-% = 5alpha_confidences X 4betas X 5gammas
+% = 5alpha X 4betas X 5gammas
 OFFSET = 100;
 alpha_confidence = 0.01; % 99% confidence
 iterations = 1000;
@@ -26,12 +28,11 @@ playersAmount = size(players,1);
 players_CI = zeros(playersAmount,4);
 
 for playerID = 0:playersAmount-1
-        
+    
     disp(['Player ' , num2str(playerID)]);
     % find player lines in model and MLE results 
     % (second condition to avoid random models where beta = 0)
     player_lines_model = find(model(:,1) == playerID  ); 
-
     player_lines_MLE = find(model_MLE(:,1) == playerID);
 
     % find best MLE full model 
@@ -42,7 +43,7 @@ for playerID = 0:playersAmount-1
     corresponding_MLE_line = p_best_MLE_line + (playerID * OFFSET);
 
     % find corresponding MLE line in model 
-    model_MLE_line = model_MLE(corresponding_MLE_line,5:end);
+    model_MLE_line = model_MLE(corresponding_MLE_line,5:end-1); % end-1 because of trailing 0
     
     % find corresponding MLE line in scrambled
     scrambled_MLE_line = zeros(1,SCRAM_NUMBER * iterations);
@@ -53,27 +54,33 @@ for playerID = 0:playersAmount-1
         scrambled_MLE = scrambled_MLE{1};
         % append the 1000 scrambled MLEs 
         current_index = 1 + ((i -1) * iterations);
-        scrambled_MLE_line(1, current_index:i*iterations) = scrambled_MLE(corresponding_MLE_line,5:end-1); % end-1 because of trailing 0???
+        scrambled_MLE_line(1, current_index:i*iterations) = scrambled_MLE(corresponding_MLE_line,5:end-1); % end-1 because of trailing 0 
     end
+    
     % structure: rows are the model MLE instances
     % columns are comparison of MLE instance with other scrambleds MLEs
-    MLE_comparison = zeros(iterations,SCRAM_NUMBER * iterations);
+    MLE_comparison = zeros(SCRAM_NUMBER * iterations);
     row_idx = 1;
     
     % binary vectors, 1 if phat+-CI > 0.5, 0 otherwise
     MLE_results_player = zeros(1,iterations);
     
+    % removed indexing for the loop. test if it works TODO
+    % check: do I need mle_comparison to be a matrix (use a vector and
+    % re-populate over and over
+    
     % logical comparison of each value from model_MLE with all scrambles
-    for MLE_instance = model_MLE_line(:,1:end-1)
+    for MLE_instance = model_MLE_line 
         if TOLERANCE > 0
-            MLE_comparison(row_idx,:) = MLE_instance < scrambled_MLE_line + (scrambled_MLE_line * COMPARISON_FACTOR);
+            MLE_comparison = MLE_instance < scrambled_MLE_line + (scrambled_MLE_line * COMPARISON_FACTOR);
         elseif TOLERANCE < 0
-            MLE_comparison(row_idx,:) = MLE_instance < scrambled_MLE_line - (scrambled_MLE_line * COMPARISON_FACTOR);
+            MLE_comparison = MLE_instance < scrambled_MLE_line - (scrambled_MLE_line * COMPARISON_FACTOR);
         elseif TOLERANCE == 0
-            MLE_comparison(row_idx,:) = MLE_instance < scrambled_MLE_line;
+            MLE_comparison = MLE_instance < scrambled_MLE_line;
         end
+        
         % apply binomial CI test (clopper-pearson) at results of comparison
-        [phat, pci] = binofit(sum(MLE_comparison(row_idx,:)),SCRAM_NUMBER * iterations,alpha_confidence);
+        [phat, pci] = binofit(sum(MLE_comparison),SCRAM_NUMBER * iterations,alpha_confidence);
         
         % if the Confidence Interval is above 0.5 threshold 
         % the MLE instance is stat. sign. better than scrambled
@@ -83,7 +90,6 @@ for playerID = 0:playersAmount-1
         
         row_idx = row_idx + 1;
     end
-    
     
     % apply binomial CI test (clopper-pearson) at MLE instances outcomes
     [phat, pci] = binofit(sum(MLE_results_player),size(MLE_results_player,2),alpha_confidence);
