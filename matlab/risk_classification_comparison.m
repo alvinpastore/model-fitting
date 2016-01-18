2d5adad88a91fbc34ae1eba9b4868b0db4d48f7b
Alvin <alvin.pastore@gmail.com>
2016-01-18 10:06:34 +0000

    updated figures making in comparison
    comparison now prints all V all, risk V all, beta V all and stddev V all
    
    also counts how many player are statistically better represented by
    each method

--- matlab/risk_classification_comparison.m
+++ matlab/risk_classification_comparison.m
@@ -1,15 +1,17 @@
 
 tic;
 RUN_ANALYSIS = 1;% 1 run the analysys. 0 only figures (after analysis done the first time)
-LOAD_MLEs = 0; % 1 if models and MLE need loading
-SINGLE_FIGURES = 0; % FLAG to plot errorbar figures singularly
-MULTI_FIGURE = 1 % FLAG to plot cumulative errorbar figure 
+LOAD_MLEs = 1; % 1 if models and MLE need loading
+SIMPLE_FIGURE = 0; % simple avgMLE value comparison
+MULTI_FIGURE = 1; % FLAG to plot cumulative errorbar figure 
+PARTIAL_FIGURES = 1; % FLAG to plot errorbar figures singularly
 % offset = amount of models in gridsearch
 % = 5alpha X 4betas X 5gammas
 OFFSET = 100;
 alpha_confidence = 0.01; % 99% confidence
 iterations = 1000;
 FONT_SIZE = 16;
+MARKER_SIZE = 10;
 
 if LOAD_MLEs
     risk_classified   = csvread('results/after_money_1k/_fullModel_2states_profit/Negative_25cap_3act_1000rep_0.1-1.0_alpha10.0-40.0_beta0.01-0.999_gamma_u.csv');
@@ -80,7 +82,7 @@ if RUN_ANALYSIS
         % structure to save the outcome of the three simple_avgMLE_comparison (rvb,rvs,bvs)
         % each row is the outcome of the simple_avgMLE_comparisons (1 if stat sign, 0 otherwise)
         MLE_results_player = zeros(6,iterations);
-
+        
         % 1,2)compare each MLE instance in risk_MLE_line vs all MLE instances in beta_MLE_line and stddev_MLE_line 
         % 3,4)compare each MLE instance in beta_MLE_line vs all MLE instances in stddev_MLE_line and risk_MLE_line
         % 5,6)compare each MLE instance in stddev_MLE_line vs all MLE instances in risk_MLE_line and beta_MLE_line 
@@ -162,7 +164,7 @@ if RUN_ANALYSIS
         players_errorbars_bvr(playerID+1,:) = [playerID, phat4, pci4(1), pci4(2)];
         players_errorbars_svb(playerID+1,:) = [playerID, phat5, pci5(1), pci5(2)];
         players_errorbars_svr(playerID+1,:) = [playerID, phat6, pci6(1), pci6(2)];
-        
+                
         % store avg MLE values and simple_avgMLE_comparison p-vaules
         simple_avgMLE_comparison(playerID + 1,:) = [risk_best_MLE beta_best_MLE stddev_best_MLE];
         
@@ -171,32 +173,121 @@ end
 close all;
 
 %% plot simple avg MLE simple_avgMLE_comparison
-fig1 = figure(1);
-hold on
-plot(1:1:playersAmount,simple_avgMLE_comparison(:,1),'bx','LineWidth',4);
-plot(1:1:playersAmount,simple_avgMLE_comparison(:,2),'ro','LineWidth',4);
-plot(1:1:playersAmount,simple_avgMLE_comparison(:,3),'g*','LineWidth',4);
-[hleg1, hobj1] = legend({'risk','beta','stddev'},'FontSize',20);
-set(hleg1,'position',[0.15 0.7 0.15 0.2])
-title('MLE simple avgMLE comparison for stock classification methods');
-ylabel('Model Fitness (MLE)');
-xlabel('Player ');
-set(gca,'XTick',[0,5,10,15,20,25,30,35,40,45],...  
-        'XTickLabel',[0,5,10,15,20,25,30,35,40,45],...
-        'FontSize',20);
-hold off
+if SIMPLE_COMPARISON_FIGURE
+    fig1 = figure();
+    hold on
+    plot(1:1:playersAmount,simple_avgMLE_comparison(:,1),'bx','LineWidth',4);
+    plot(1:1:playersAmount,simple_avgMLE_comparison(:,2),'ro','LineWidth',4);
+    plot(1:1:playersAmount,simple_avgMLE_comparison(:,3),'g*','LineWidth',4);
+    [hleg1, hobj1] = legend({'risk','beta','stddev'},'FontSize',20);
+    set(hleg1,'position',[0.18 0.68 0.15 0.2])
+    title('MLE simple avgMLE comparison for stock classification methods');
+    ylabel('Model Fitness (MLE)');
+    xlabel('Player ');
+    set(gca,'XTick',[0,5,10,15,20,25,30,35,40,45],...  
+            'XTickLabel',[0,5,10,15,20,25,30,35,40,45],...
+            'FontSize',20);
+    hold off
+end
 
 %% errorbars graphs %%
 
-if SINGLE_FIGURES 
-    fig2 = figure(2);
+if MULTI_FIGURE
+    %% graph for all comparisons errorbars together (sorted according to rvb)
+    fig2 = figure();
     hold on
-    [sorted_CI_rvb, sort_idx] = sortrows(players_errorbars_rvb,2);
-    % x axis is the same for all (1:1:46) but the actual order is in sort_idx
-    errorbar(players_errorbars_rvb(:,1),sorted_CI_rvb(:,2),...
-        sorted_CI_rvb(:,2)-sorted_CI_rvb(:,3),...
-        sorted_CI_rvb(:,4)-sorted_CI_rvb(:,2));
-    title('Risk vs Beta','FontSize',FONT_SIZE);
+    
+    title('Risk v Beta vs Std Dev','FontSize',FONT_SIZE);
+    plot([0,46],[0.5,0.5],'r-');
+    axis([-1 46 -0.05 1.05]);
+    labels = num2str(sorted_CI_bvs(:,1));
+    set(gca,'Xtick',0:1:45,'XTickLabel',labels);
+    xlabel('Player ID');
+    ylabel('Probability');
+    set(gca,'FontSize',FONT_SIZE);
+    set(gca,'xgrid','on')
+    
+    [sorted_CI_rvb, sort_idx] = sortrows(players_errorbars_rvb,1);  
+    sorted_CI_rvs = sortrows(players_errorbars_rvs,2);
+    sorted_CI_bvs = sortrows(players_errorbars_bvs,2);
+    sorted_CI_bvr = sortrows(players_errorbars_bvr,2);
+    sorted_CI_svb = sortrows(players_errorbars_svb,2);
+    sorted_CI_svr = sortrows(players_errorbars_svr,2);
+    
+    e1 = errorbar(players_errorbars_rvb(:,1),sorted_CI_rvb(sort_idx,2),...
+        sorted_CI_rvb(sort_idx,2)-sorted_CI_rvb(sort_idx,3),...
+        sorted_CI_rvb(sort_idx,4)-sorted_CI_rvb(sort_idx,2),'xr','MarkerSize',MARKER_SIZE);
+
+    e2 = errorbar(players_errorbars_rvb(:,1),sorted_CI_rvs(sort_idx,2),...
+        sorted_CI_rvs(sort_idx,2)-sorted_CI_rvs(sort_idx,3),...
+        sorted_CI_rvs(sort_idx,4)-sorted_CI_rvs(sort_idx,2),'or','MarkerSize',MARKER_SIZE);
+
+    e3 = errorbar(players_errorbars_rvb(:,1),sorted_CI_bvs(sort_idx,2),...
+        sorted_CI_bvs(sort_idx,2)-sorted_CI_bvs(sort_idx,3),...
+        sorted_CI_bvs(sort_idx,4)-sorted_CI_bvs(sort_idx,2),'*b','MarkerSize',MARKER_SIZE);
+
+    e4 = errorbar(players_errorbars_rvb(:,1),sorted_CI_bvr(sort_idx,2),...
+        sorted_CI_bvr(sort_idx,2)-sorted_CI_bvr(sort_idx,3),...
+        sorted_CI_bvr(sort_idx,4)-sorted_CI_bvr(sort_idx,2),'xb','MarkerSize',MARKER_SIZE);
+
+    e5 = errorbar(players_errorbars_rvb(:,1),sorted_CI_svb(sort_idx,2),...
+        sorted_CI_svb(sort_idx,2)-sorted_CI_svb(sort_idx,3),...
+        sorted_CI_svb(sort_idx,4)-sorted_CI_svb(sort_idx,2),'*g','MarkerSize',MARKER_SIZE);
+
+    e6 = errorbar(players_errorbars_rvb(:,1),sorted_CI_svr(sort_idx,2),...
+        sorted_CI_svr(sort_idx,2)-sorted_CI_svr(sort_idx,3),...
+        sorted_CI_svr(sort_idx,4)-sorted_CI_svr(sort_idx,2),'og','MarkerSize',MARKER_SIZE);
+        
+    hleg = legend([e1 e2 e3 e4 e5 e6],...
+        {'Risk v Beta','Risk v Stddev', 'Beta v Stddev',...
+        'Beta v Risk','Stddev v Beta','Stddev v Risk'});
+    
+    set(hleg,'position',[0.18,0.68,0.2,0.2]) ;
+    hold off
+end
+
+
+if PARTIAL_FIGURES 
+    best_measure_count = zeros(1,3);
+    for playerID = 1:playersAmount
+                
+        % compare risk vs beta AND stddev
+        if min(players_errorbars_rvb(playerID,2:end)) > 0.5 &&...
+           min(players_errorbars_rvs(playerID,2:end)) > 0.5 
+            best_measure_count(1) = best_measure_count(1) + 1;
+        end
+        
+        % compare beta vs risk AND stddev
+        if min(players_errorbars_bvr(playerID,2:end)) > 0.5 &&...
+           min(players_errorbars_bvs(playerID,2:end)) > 0.5
+            best_measure_count(2) = best_measure_count(2) + 1;
+        end
+        
+        % compare stddev vs risk AND beta
+        if min(players_errorbars_svr(playerID,2:end)) > 0.5 &&...
+           min(players_errorbars_svb(playerID,2:end)) > 0.5
+            best_measure_count(3) = best_measure_count(3) + 1;
+        end
+    end
+    
+    disp(['risk ',num2str(best_measure_count(1)),...
+          ' beta ',num2str(best_measure_count(2)),...
+          ' stddev ',num2str(best_measure_count(3))]);
+    
+    % figure risk vs rest
+    fig3 = figure();
+    hold on
+    [sorted_CI_rvb, sort_idx1] = sortrows(players_errorbars_rvb,2);
+
+    errorbar(players_errorbars_rvb(:,1),players_errorbars_rvb(sort_idx1,2),...
+        players_errorbars_rvb(sort_idx1,2)-players_errorbars_rvb(sort_idx1,3),...
+        players_errorbars_rvb(sort_idx1,4)-players_errorbars_rvb(sort_idx1,2));
+    
+    errorbar(players_errorbars_rvb(:,1),players_errorbars_rvs(sort_idx1,2),...
+        players_errorbars_rvs(sort_idx1,2)-players_errorbars_rvs(sort_idx1,3),...
+        players_errorbars_rvs(sort_idx1,4)-players_errorbars_rvs(sort_idx1,2));
+    
+    title('Risk vs Beta and Stddev','FontSize',FONT_SIZE);
     plot([0,46],[0.5,0.5],'r-');
     axis([-1 46 0 1]);
     labels = num2str(sorted_CI_rvb(:,1));
@@ -205,84 +296,52 @@ if SINGLE_FIGURES
     ylabel('Probability');
     set(gca,'FontSize',FONT_SIZE);
     hold off
-
-
-    fig3 = figure(3);
+    
+    % figure beta vs rest
+    fig4 = figure();
     hold on
-    sorted_CI_rvs = sortrows(players_errorbars_rvs,2);
-    errorbar(players_errorbars_rvb(:,1),sorted_CI_rvs(:,2),...
-        sorted_CI_rvs(:,2)-sorted_CI_rvs(:,3),...
-        sorted_CI_rvs(:,4)-sorted_CI_rvs(:,2));
-    title('Risk vs Std Dev','FontSize',FONT_SIZE);
+    [sorted_CI_bvr,sort_idx2] = sortrows(players_errorbars_bvr,2);
+    
+    errorbar(players_errorbars_bvr(:,1),players_errorbars_bvr(sort_idx2,2),...
+        players_errorbars_bvr(sort_idx2,2)-players_errorbars_bvr(sort_idx2,3),...
+        players_errorbars_bvr(sort_idx2,4)-players_errorbars_bvr(sort_idx2,2));
+    
+    errorbar(players_errorbars_bvr(:,1),players_errorbars_bvs(sort_idx2,2),...
+        players_errorbars_bvs(sort_idx2,2)-players_errorbars_bvs(sort_idx2,3),...
+        players_errorbars_bvs(sort_idx2,4)-players_errorbars_bvs(sort_idx2,2));
+    
+    title('Beta vs Risk and Std dev','FontSize',FONT_SIZE);
     plot([0,46],[0.5,0.5],'r-');
     axis([-1 46 0 1]);
-    labels = num2str(sorted_CI_rvs(:,1));
+    labels = num2str(sorted_CI_bvr(:,1));
     set(gca,'Xtick',0:1:45,'XTickLabel',labels);
     xlabel('Player ID');
     ylabel('Probability');
     set(gca,'FontSize',FONT_SIZE);
     hold off
 
-
-
-    fig4 = figure(4);
+    % figure stddev vs rest
+    fig5 = figure();
     hold on
-    sorted_CI_bvs = sortrows(players_errorbars_bvs,2);
-    errorbar(players_errorbars_rvb(:,1),sorted_CI_bvs(:,2),...
-        sorted_CI_bvs(:,2)-sorted_CI_bvs(:,3),...
-        sorted_CI_bvs(:,4)-sorted_CI_bvs(:,2));
-    title('Beta vs Std Dev','FontSize',FONT_SIZE);
+    [sorted_CI_svr, sort_idx3] = sortrows(players_errorbars_svb,2);
+    
+    errorbar(players_errorbars_svr(:,1),players_errorbars_svb(sort_idx3,2),...
+        players_errorbars_svb(sort_idx3,2)-players_errorbars_svb(sort_idx3,3),...
+        players_errorbars_svb(sort_idx3,4)-players_errorbars_svb(sort_idx3,2));  
+    
+    errorbar(players_errorbars_svr(:,1),players_errorbars_svr(sort_idx3,2),...
+        players_errorbars_svr(sort_idx3,2)-players_errorbars_svr(sort_idx3,3),...
+        players_errorbars_svr(sort_idx3,4)-players_errorbars_svr(sort_idx3,2));      
+    
+    title('Std Dev vs Risk and Beta','FontSize',FONT_SIZE);
     plot([0,46],[0.5,0.5],'r-');
     axis([-1 46 0 1]);
-    labels = num2str(sorted_CI_bvs(:,1));
+    labels = num2str(sorted_CI_svr(:,1));
     set(gca,'Xtick',0:1:45,'XTickLabel',labels);
     xlabel('Player ID');
     ylabel('Probability');
     set(gca,'FontSize',FONT_SIZE);
     hold off
 end
-
-%% graph for all comparisons errorbars together (sorted according to rvb)
-fig5 = figure(5);
-hold on
-[sorted_CI_rvb, sort_idx] = sortrows(players_errorbars_rvb,1);
-
-e1 = errorbar(players_errorbars_rvb(:,1),sorted_CI_rvb(:,2),...
-    sorted_CI_rvb(:,2)-sorted_CI_rvb(:,3),...
-    sorted_CI_rvb(:,4)-sorted_CI_rvb(:,2),'x');
-
-e2 = errorbar(players_errorbars_rvb(:,1),sorted_CI_rvs(sort_idx,2),...
-    sorted_CI_rvs(sort_idx,2)-sorted_CI_rvs(sort_idx,3),...
-    sorted_CI_rvs(sort_idx,4)-sorted_CI_rvs(sort_idx,2),'x');
-
-e3 = errorbar(players_errorbars_rvb(:,1),sorted_CI_bvs(sort_idx,2),...
-    sorted_CI_bvs(sort_idx,2)-sorted_CI_bvs(sort_idx,3),...
-    sorted_CI_bvs(sort_idx,4)-sorted_CI_bvs(sort_idx,2),'x');
-
-e4 = errorbar(players_errorbars_rvb(:,1),sorted_CI_bvs(sort_idx,2),...
-    sorted_CI_bvs(sort_idx,2)-sorted_CI_bvs(sort_idx,3),...
-    sorted_CI_bvs(sort_idx,4)-sorted_CI_bvs(sort_idx,2),'x');
-
-e5 = errorbar(players_errorbars_rvb(:,1),sorted_CI_bvs(sort_idx,2),...
-    sorted_CI_bvs(sort_idx,2)-sorted_CI_bvs(sort_idx,3),...
-    sorted_CI_bvs(sort_idx,4)-sorted_CI_bvs(sort_idx,2),'x');
-
-e6 = errorbar(players_errorbars_rvb(:,1),sorted_CI_bvs(sort_idx,2),...
-    sorted_CI_bvs(sort_idx,2)-sorted_CI_bvs(sort_idx,3),...
-    sorted_CI_bvs(sort_idx,4)-sorted_CI_bvs(sort_idx,2),'x');
-
-legend([e1 e2 e3], {'Risk v Beta','Risk v Stddev', 'Beta v Stddev'});
-
-title('Beta vs Std Dev','FontSize',FONT_SIZE);
-plot([0,46],[0.5,0.5],'r-');
-axis([-1 46 0 1]);
-labels = num2str(sorted_CI_bvs(:,1));
-set(gca,'Xtick',0:1:45,'XTickLabel',labels);
-xlabel('Player ID');
-ylabel('Probability');
-set(gca,'FontSize',FONT_SIZE);
-set(gca,'xgrid','on')
-hold off
-
 
 toc;
