f6e3304649f5a5cb5815b481422e7d7f35975431
Alvin <alvin.pastore@gmail.com>
2016-02-10 16:06:49 +0000

    closed the function statement and added proper indentation

--- matlab/MLE_SCRAM_importer.m
+++ matlab/MLE_SCRAM_importer.m
@@ -1,33 +1,34 @@
 function [Num, MLESCRAMS, MLEFULL, res3] = MLE_SCRAM_importer(scram_flag)
 
-%script to import all the MLE files (20mins each approx by manual import)
+    %script to import all the MLE files (20mins each approx by manual import)
 
-tic;
+    tic;
 
-% import model CSV MLE 
-MLEFULL = csvread('results/MLE_model/MLE_Portfolio_[0.01, 0.25, 0.5, 0.75, 0.999]_1000_u.csv');
-res3 = csvread('results/after_money_1k/_fullModel_2states_profit/Negative_Portfolio_25cap_3act_1000rep_0.1-1.0_alpha10.0-40.0_beta0.01-0.999_gamma_u.csv');
-% count the scrambled MLE files in the experiment folder
-FolderName = 'results/MLE_scram_matlab/';
-D = dir([FolderName, '/MLE_*.csv']);
-Num = length(D(not([D.isdir])));
+    % import model CSV MLE 
+    MLEFULL = csvread('results/MLE_model/MLE_Portfolio_[0.01, 0.25, 0.5, 0.75, 0.999]_1000_u.csv');
+    res3 = csvread('results/after_money_1k/_fullModel_2states_profit/Negative_Portfolio_25cap_3act_1000rep_0.1-1.0_alpha10.0-40.0_beta0.01-0.999_gamma_u.csv');
+    % count the scrambled MLE files in the experiment folder
+    FolderName = 'results/MLE_scram_matlab/';
+    D = dir([FolderName, '/MLE_*.csv']);
+    Num = length(D(not([D.isdir])));
 
 
-% cell array to keep all the MLE matrices
-MLESCRAMS = cell(Num,1);
-    
-% goes through scrams only if the input flag is true
-if scram_flag
-    % Import each file in the experiment directory in the MLESCRAMS cell array
-    for i = 1:Num
-        MLESCRAMS{i} = csvread([FolderName,D(i).name]);
-        disp(D(i).name);
+    % cell array to keep all the MLE matrices
+    MLESCRAMS = cell(Num,1);
 
+    % goes through scrams only if the input flag is true
+    if scram_flag
+        % Import each file in the experiment directory in the MLESCRAMS cell array
+        for i = 1:Num
+            MLESCRAMS{i} = csvread([FolderName,D(i).name]);
+            disp(D(i).name);
+
+        end
     end
+
+    varlist = {'FolderName', 'D', 'i'};
+    clear (varlist{:});
+    clear varlist;
+
+    toc
 end
-
-varlist = {'FolderName', 'D', 'i'};
-clear (varlist{:});
-clear varlist;
-
-toc
