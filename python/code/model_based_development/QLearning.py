e527b75055270a1e1daa97f90ceb48f5d5036ef8
Alvin <alvin.pastore@gmail.com>
2016-04-13 15:37:57 +0100

    standardised QLearning (lowercase variables and newline at the end of file)

--- python/code/model_based_development/QLearning.py
+++ python/code/model_based_development/QLearning.py
@@ -1,9 +1,10 @@
 
 from ReinforcementLearningModel import ReinforcementLearningModel
 
+
 class QLearning(ReinforcementLearningModel):
 
-    def update_Q(self,state, next_state, action, reward, alpha, gamma):
-        TD_error = (reward + (gamma * max(self.Q[next_state])) - self.Q[state][action])
+    def update_Q(self, state, next_state, action, reward, alpha, gamma):
+        td_error = (reward + (gamma * max(self.Q[next_state])) - self.Q[state][action])
         # state is the linear index of the current state
-        self.Q[state][action] += + alpha * TD_error
+        self.Q[state][action] += + alpha * td_error
