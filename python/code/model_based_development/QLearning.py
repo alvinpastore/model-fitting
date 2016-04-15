86b67bf07460dec5ea924892a17c35f1aa6a9e82
Alvin <alvin.pastore@gmail.com>
2016-04-15 12:07:08 +0100

    reorganised method signature

--- python/code/model_based_development/QLearning.py
+++ python/code/model_based_development/QLearning.py
@@ -1,10 +1,8 @@
-
 from ReinforcementLearningModel import ReinforcementLearningModel
 
 
 class QLearning(ReinforcementLearningModel):
 
-    def update_Q(self, state, next_state, action, reward, alpha, gamma):
-        td_error = (reward + (gamma * max(self.Q[next_state])) - self.Q[state][action])
-        # state is the linear index of the current state
-        self.Q[state][action] += + alpha * td_error
+    def update_Q(self, state, next_state, alpha, gamma, k, nStates, nAction):
+        td_error = (self.reward + (gamma * max(self.Q[next_state])) - self.Q[state][self.action])
+        self.Q[state][self.action] += + alpha * td_error
--- python/code/model_based_development/Sarsa.py
+++ python/code/model_based_development/Sarsa.py
@@ -3,8 +3,7 @@ from ReinforcementLearningModel import ReinforcementLearningModel
 
 class Sarsa(ReinforcementLearningModel):
 
-    def update_Q(self,state, next_state, action, reward, alpha, gamma):
+    def update_Q(self, state, next_state, alpha, gamma, k, nStates, nAction):
         next_action = self.pick_random_best_action(self.Q[next_state])
-        td_error = reward + (gamma * self.Q[next_state][next_action]) - self.Q[state][action]
-        # state is the linear index of the current state
-        self.Q[state][action] += + alpha * td_error
+        td_error = self.reward + (gamma * self.Q[next_state][next_action]) - self.Q[state][self.action]
+        self.Q[state][self.action] += + alpha * td_error
