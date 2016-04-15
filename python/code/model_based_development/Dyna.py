a9dfe618360ad104791ff79e1dccf2cc05ea1724
Alvin <alvin.pastore@gmail.com>
2016-04-15 11:14:52 +0100

    Dyna lower case + update model before updating qvalues withing the update_Q function (to make the episode consistent and modularise the execution)

--- python/code/model_based_development/Dyna.py
+++ python/code/model_based_development/Dyna.py
@@ -7,16 +7,20 @@ class Dyna(ReinforcementLearningModel):
     R = []
     T = []
 
-    def __init__(self, n_actions, n_states, initial_Q):
-        self.Q = [[initial_Q for x1 in xrange(n_actions)] for x2 in xrange(n_states)]
-        self.R = [ [0 for x1 in xrange(n_actions)] for x2 in xrange(n_states)]
-        self.T = [[[0 for x0 in xrange(n_states)]  for x1 in xrange(n_actions)] for x2 in xrange(n_states)]
+    def __init__(self, n_actions, n_states, initial_q):
+        self.Q = [[initial_q for x1 in xrange(n_actions)] for x2 in xrange(n_states)]
+        self.R = [[0 for x1 in xrange(n_actions)] for x2 in xrange(n_states)]
+        self.T = [[[0 for x0 in xrange(n_states)] for x1 in xrange(n_actions)] for x2 in xrange(n_states)]
 
-    def update_Q(self, state, action, alpha, gamma, k, nStates, nActions):
+    def update_Q(self, state, next_state, alpha, gamma, k, nStates, nActions, action):
+
+        # Update model of the world first
+        self.update_model(state, next_state, action, alpha, self.reward)
+
         # Q-Values update for experienced state-action pair
         # state is the linear index of the current state
-        sum_transitions_max_Q = self.calculate_sum_transitions_max_Q(state,action,alpha)
-        self.Q[state][action] = self.R[state][action] + (gamma * sum_transitions_max_Q)
+        sum_transitions_max_q = self.calculate_sum_transitions_max_q(state, action, alpha)
+        self.Q[state][action] = self.R[state][action] + (gamma * sum_transitions_max_q)
 
         # Dyna update of k random state-action pairs
         for i in range(k):
@@ -25,15 +29,15 @@ class Dyna(ReinforcementLearningModel):
             # print 'updating pair {0},{1}'.format(s_rand,a_rand)
 
             # update that couple using T(s_rand,a_rand,all_landing states)*max(Q[landing_state])
-            sum_transitions_max_Q = self.calculate_sum_transitions_max_Q(s_rand,a_rand,alpha)
-            self.Q[s_rand][a_rand] = self.R[s_rand][a_rand] + (gamma * sum_transitions_max_Q)
+            sum_transitions_max_q = self.calculate_sum_transitions_max_q(s_rand, a_rand, alpha)
+            self.Q[s_rand][a_rand] = self.R[s_rand][a_rand] + (gamma * sum_transitions_max_q)
 
-    def calculate_sum_transitions_max_Q(self, state, action, alpha):
-        sum_transitions_max_Q = 0
+    def calculate_sum_transitions_max_q(self, state, action, alpha):
+        sum_transitions_max_q = 0
         for state_t in xrange(len(self.T[state][action])):
             self.T[state][action][state_t] += alpha * (0 - self.T[state][action][state_t])
-            sum_transitions_max_Q += self.T[state][action][state_t] * max(self.Q[state_t])
-        return sum_transitions_max_Q
+            sum_transitions_max_q += self.T[state][action][state_t] * max(self.Q[state_t])
+        return sum_transitions_max_q
 
     def update_model(self, state, next_state, action, alpha, reward):
         # state and next_state are the linear index of each state
@@ -43,7 +47,7 @@ class Dyna(ReinforcementLearningModel):
         # update the Transition Probability of the tuple T(s,a,s_t+1)
         self.T[state][action][next_state] += alpha * (1 - self.T[state][action][next_state])
         # decay the T(s,a,s') for all s'
-        self.calculate_sum_transitions_max_Q(state, action, alpha)
+        self.calculate_sum_transitions_max_q(state, action, alpha)
 
     def print_model(self):
         print 'R'
