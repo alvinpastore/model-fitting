6b4581b8792ec96ad4e25865975133cd51bbf693
Alvin <alvin.pastore@gmail.com>
2016-04-15 11:14:12 +0100

    Sarsa 3 lower case

--- python/code/model_based_development/Sarsa.py
+++ python/code/model_based_development/Sarsa.py
@@ -1,9 +1,10 @@
 from ReinforcementLearningModel import ReinforcementLearningModel
 
+
 class Sarsa(ReinforcementLearningModel):
 
     def update_Q(self,state, next_state, action, reward, alpha, gamma):
         next_action = self.pick_random_best_action(self.Q[next_state])
-        TD_error = reward + (gamma * self.Q[next_state][next_action]) - self.Q[state][action]
+        td_error = reward + (gamma * self.Q[next_state][next_action]) - self.Q[state][action]
         # state is the linear index of the current state
-        self.Q[state][action] += + alpha * TD_error
+        self.Q[state][action] += + alpha * td_error
