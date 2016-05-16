13b5eb21e539cda4b043e43266c3b4b497539954
Alvin <alvin.pastore@gmail.com>
2016-05-16 14:29:47 +0100

    need to specify number of repetitions of file to import

--- matlab/MLE_model_importer.m
+++ matlab/MLE_model_importer.m
@@ -1,21 +1,21 @@
-function [ MLEFULL, model ] = MLE_model_importer( model_type , k)
+function [ MLEFULL, model ] = MLE_model_importer( model_type , rep, k)
 %MLE_MODEL_IMPORTER loads models and MLE csv files 
 %for both model free and model based
 
     if strcmpi(model_type,'model_free')
         
-        MLEFULL = csvread('../results/MLE_model/beta_classified/MLE_Portfolio_[0.01, 0.25, 0.5, 0.75, 0.999]_1000_u.csv');
-        model = csvread('../results/after_money_1k/_fullModel_2states_profit/Negative_Portfolio_25cap_3act_1000rep_0.1-1.0_alpha10.0-40.0_beta0.01-0.999_gamma_u.csv');
-    
+        MLEFULL = csvread(['../results/MLE_model/beta_classified/MLE_Portfolio_[0.01, 0.25, 0.5, 0.75, 0.999]_',num2str(rep),'_u.csv']);
+        model = csvread(['../results/after_money_1k/_beta_classified/Negative_Portfolio_25cap_3act_',num2str(rep),'rep_0.1-1.0_alpha10.0-40.0_beta0.01-0.999_gamma_u.csv']);
+        %_fullModel_2states_profit
     elseif strcmpi(model_type,'model_based')
         
-        MLEFULL = csvread(['../results/MLE_model/model_based/MLE_ModelBased_[0.01, 0.25, 0.5, 0.75, 0.999]_1000_u_',num2str(k),'k.csv']);
-        model = csvread(['../results/after_money_1k/_model_based/ModelBased_25cap_3act_1000rep_0.1-1.0_alpha10.0-40.0_beta0.01-0.999_gamma_u_',num2str(k),'k.csv']);
+        MLEFULL = csvread(['../results/MLE_model/model_based/MLE_ModelBased_[0.01, 0.25, 0.5, 0.75, 0.999]_',num2str(rep),'_u_',num2str(k),'k.csv']);
+        model = csvread(['../results/after_money_1k/_model_based/ModelBased_25cap_3act_',num2str(rep),'rep_0.1-1.0_alpha10.0-40.0_beta0.01-0.999_gamma_u_',num2str(k),'k.csv']);
     
     elseif strcmpi(model_type,'no_gamma')
         
-        MLEFULL = csvread('../results/MLE_model/nogamma/MLE_Portfolio_[0.0]_1000_u.csv');
-        model = csvread('../results/after_money_1k/_nogamma/profit_states/Negative_Portfolio_25cap_3act_1000rep_0.1-1.0_alpha10.0-40.0_beta0.0-0.0_gamma_u.csv');
+        MLEFULL = csvread(['../results/MLE_model/nogamma/MLE_Portfolio_[0.0]_',num2str(rep),'_u.csv']);
+        model = csvread(['../results/after_money_1k/_nogamma/profit_states/Negative_Portfolio_25cap_3act_',num2str(rep),'rep_0.1-1.0_alpha10.0-40.0_beta0.0-0.0_gamma_u.csv']);
         
     end
 end
