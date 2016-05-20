2923e800699178821c4ac0d6a726f8e2491405c4
Alvin <alvin.pastore@gmail.com>
2016-05-20 12:27:32 +0100

    Creates a histogram for each players best model free  plotting the distribution of the MLEs

--- matlab/clt_MLE.m
+++ matlab/clt_MLE.m
@@ -1,14 +1,41 @@
-% creates a histogram for each line in MLE files 
+
+
+%% creates a histogram for each players best model free 
 % plotting the distribution of the MLEs
-% this was used to investigate the distribution of MLEs (Central Limit Theorem) 
+% used to investigate the distribution of MLEs (Central Limit Theorem) 
+close all;
 
-mle = csvread('results/MLE_model/MLE_Portfolio_[0.01, 0.25, 0.5, 0.75, 0.999]_1000_u.csv');
-%mle = csvread('results/MLE_model/MLE_ModelBased_[0.01, 0.25, 0.5, 0.75, 0.999]_1000_u.csv');
+% offset = amount of models in gridsearch
+% = 5alpha X 4betas X 5gammas
+OFFSET = 100;
+REPETITIONS = 5000;
+BINS_NUMBER = REPETITIONS/10;
 
-for i = 1: length(mle)
-    figure(i);
-    mles = mle(i,5:); % skips columns 1-4 (PID and parameters) 
-    hist(mles);
-    %axis([0 50 0 600]);
-    disp(i);
+
+%[MF_MLE, model_free] = MLE_model_importer('model_free',REPETITIONS);
+model_free = model_free(model_free(:,2) ~= 0,:);  
+
+% count players
+players = unique(model_free(:,1));
+playersAmount = size(players,1);
+mode_mean = zeros(playersAmount,2);
+
+for playerID = 0:playersAmount-1
+    
+
+    
+    % find player lines in model and MLE results for model free
+    player_lines_model_free = find(model_free(:,1) == playerID); 
+    [MF_best_MLE, MF_best_MLE_line] = min(model_free(player_lines_model_free,5));
+    % MF_best_MLE_line is relative to the lines of playerID
+    % the offset is the amount of lines (100) for each player
+    corresponding_MLE_line = MF_best_MLE_line + (playerID * OFFSET);
+    % find corresponding MLE line in model 
+    model_free_MLE_line = MF_MLE(corresponding_MLE_line,5:end);
+    
+    figure()
+    hist(model_free_MLE_line,BINS_NUMBER);
+    disp(['Player ' , num2str(playerID)]);
+    mode_mean(playerID + 1,:) = [mode(model_free_MLE_line) , mean(model_free_MLE_line)];
+    
 end
