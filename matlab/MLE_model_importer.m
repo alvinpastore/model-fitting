1d2d864cc40a318295b76dbb8d358aa78669d03e
Alvin <alvin.pastore@gmail.com>
2016-05-31 13:10:25 +0100

    fixed bug (folder used for model free was beta_classified instead of risk_classified)

--- matlab/MLE_model_importer.m
+++ matlab/MLE_model_importer.m
@@ -5,8 +5,8 @@ function [ MLEFULL, model ] = MLE_model_importer( model_type , rep, k)
     tic
     if strcmpi(model_type,'model_free')
         
-        MLEFULL = csvread(['../results/MLE_model/beta_classified/MLE_Portfolio_[0.01, 0.25, 0.5, 0.75, 0.999]_',num2str(rep),'_u.csv']);
-        model = csvread(['../results/after_money_1k/_beta_classified/Negative_Portfolio_25cap_3act_',num2str(rep),'rep_0.1-1.0_alpha10.0-40.0_beta0.01-0.999_gamma_u.csv']);
+        MLEFULL = csvread(['../results/MLE_model/risk_classified/MLE_Portfolio_[0.01, 0.25, 0.5, 0.75, 0.999]_',num2str(rep),'_u.csv']);
+        model = csvread(['../results/after_money_1k/_risk_classified/Negative_Portfolio_25cap_3act_',num2str(rep),'rep_0.1-1.0_alpha10.0-40.0_beta0.01-0.999_gamma_u.csv']);
         %_fullModel_2states_profit
     elseif strcmpi(model_type,'model_based')
         
