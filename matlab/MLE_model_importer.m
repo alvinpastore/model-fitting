4d272c9a61d6419e81313efda433bd264012bad5
Alvin <alvin.pastore@gmail.com>
2016-06-07 12:05:24 +0100

    adapted to new results filename, will need to be updated again.

--- matlab/MLE_model_importer.m
+++ matlab/MLE_model_importer.m
@@ -5,8 +5,10 @@ function [ MLEFULL, model ] = MLE_model_importer( model_type , rep, k)
     tic
     if strcmpi(model_type,'model_free')
         
-        MLEFULL = csvread(['../results/MLE_model/risk_classified/MLE_Portfolio_[0.01, 0.25, 0.5, 0.75, 0.999]_',num2str(rep),'_u.csv']);
-        model = csvread(['../results/after_money_1k/_risk_classified/Negative_Portfolio_25cap_3act_',num2str(rep),'rep_0.1-1.0_alpha10.0-40.0_beta0.01-0.999_gamma_u.csv']);
+        filename = ['MLE_REAL_Portfolio_[0.0, 0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.999]_',num2str(rep),'_u.csv'];
+        MLEFULL = csvread(['../results/MLE_model/risk_classified/',filename]);
+        filename = ['Negative_Portfolio_REAL_25cap_3act_',num2str(rep),'rep_0.01-1.0_alpha1.0-100.0_beta0.0-0.999_gamma_u.csv'];
+        model = csvread(['../results/after_money_1k/_risk_classified/',filename]);
         %_fullModel_2states_profit
     elseif strcmpi(model_type,'model_based')
         
