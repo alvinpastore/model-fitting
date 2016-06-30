2a79229179a2de949c73a19de00b1408733c9af5
Alvin <alvin.pastore@gmail.com>
2016-06-30 16:08:58 +0100

    model importer adapted to the gradient descent version of the results

--- matlab/MLE_model_importer.m
+++ matlab/MLE_model_importer.m
@@ -1,21 +1,38 @@
-function [ MLEFULL, model ] = MLE_model_importer( model_type , cap, act, date, k)
-%MLE_MODEL_IMPORTER loads models and MLE csv files 
-%for both model free and model based
-    disp(['importing model: ',model_type]);
+function [ model ] = MLE_model_importer( restricted , algorithm, CAP, nActions)
+    %% script to import best model MLE and parameters generated via gradient descent
+    % restricted    = the number of transactions skipped for the MLE calculation. 
+    %                 using 0 will automatically search in un_restricted folder.
+    % algorithm     = the algorithm used to generated the MLEs (qlearning/sarsa)
+    % CAP           = transactions CAP
+    % nActions      = number of actions 
     tic
-    if strcmpi(model_type,'model_free')
-        
-        filename = ['MLE_REAL_Portfolio_',num2str(cap),'cap_',num2str(act),'act_u_',date,'.csv'];
-        MLEFULL = csvread(['../results/MLE_model/risk_classified/',filename]);
-        filename = ['Negative_Portfolio_REAL_',num2str(cap),'cap_3act_1rep_u_',date,'.csv'];
-        model = csvread(['../results/after_money_1k/_risk_classified/',filename]);
-        %_fullModel_2states_profit
-    elseif strcmpi(model_type,'model_based')
-        
-        %TODO still needs to adapt to new Model based filename
-        MLEFULL = csvread(['../results/MLE_model/model_based/MLE_ModelBased_[0.01, 0.25, 0.5, 0.75, 0.999]_',num2str(CAP),'_u_',num2str(k),'k.csv']);
-        model = csvread(['../results/after_money_1k/_model_based/ModelBased_25cap_3act_',num2str(CAP),'rep_0.1-1.0_alpha10.0-40.0_beta0.01-0.999_gamma_u_',num2str(k),'k.csv']);
-        
+    
+    if restricted == 0
+        restricted = 'un_';
+    else
+        restricted = num2str(restricted);
     end
+    
+    folder_path = ['../results/gradient_descent/',restricted,'restricted/',algorithm,'/'];
+    file_path = [folder_path, 'grad_desc_',num2str(CAP),'CAP_',num2str(nActions),'act.csv'];
+    disp(['importing model: ',file_path]);
+    model = csvread(file_path);
+        
     toc
 end
+
+% % OLD VERSION, before gradient descent and deterministic MLE
+% if strcmpi(model_type,'model_free')
+% 
+%     filename = ['MLE_REAL_Portfolio_',num2str(cap),'cap_',num2str(act),'act_u_',date,'.csv'];
+%     MLEFULL = csvread(['../results/MLE_model/risk_classified/',filename]);
+%     filename = ['Negative_Portfolio_REAL_',num2str(cap),'cap_3act_1rep_u_',date,'.csv'];
+%     model = csvread(['../results/after_money_1k/_risk_classified/',filename]);
+%     %_fullModel_2states_profit
+% elseif strcmpi(model_type,'model_based')
+% 
+%     %TODO still needs to adapt to new Model based filename
+%     MLEFULL = csvread(['../results/MLE_model/model_based/MLE_ModelBased_[0.01, 0.25, 0.5, 0.75, 0.999]_',num2str(CAP),'_u_',num2str(k),'k.csv']);
+%     model = csvread(['../results/after_money_1k/_model_based/ModelBased_25cap_3act_',num2str(CAP),'rep_0.1-1.0_alpha10.0-40.0_beta0.01-0.999_gamma_u_',num2str(k),'k.csv']);
+% 
+% end
