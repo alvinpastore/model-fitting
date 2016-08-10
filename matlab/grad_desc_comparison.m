87b92a88a2fb5bd3609ec63dab8bc5a0ad9e914b
Alvin <alvin.pastore@gmail.com>
2016-08-10 11:23:05 +0100

    Added the Likelihood Ratio Test figures

--- matlab/grad_desc_comparison.m
+++ matlab/grad_desc_comparison.m
@@ -3,9 +3,11 @@
 % RL model vs NoGamma model
 % RL model vs Random model
 function [better_than_random,model] = grad_desc_comparison(RESTRICTED,ALGORITHM,CAP,N_ACTIONS,RISK_MEASURE,SAVE_FIG)
-
+    close all;
+    
     % markup for figures
     MARKER_SIZE = 15;
+    THRESHOLD_SIZE = 5;
     FONT_SIZE = 20;
     PRINT_WIDTH = 80;
     PRINT_HEIGHT = 50;
@@ -13,11 +15,13 @@ function [better_than_random,model] = grad_desc_comparison(RESTRICTED,ALGORITHM,
     % number of params of bigger model - number of params of nested model
     % nogamma vs random
     ngm_rnd_dof = 2-0;  % alpha, beta VS random(no param)
+    threshold_2dof = 5.991;
     % rl model vs nogamma
     rlm_ngm_dof = 3-2;  % alpha, beta, gamma VS alpha, beta
+    threshold_1dof = 3.841;
     % rl model vs random
     rlm_rnd_dof = 3-0;  % alpha, beta, gamma VS random(no param)
-    
+    threshold_3dof = 7.815;
     % the original amount of transactions
     % depends only on the CAP (not restriction yet)
     transactions_number = csvread(['../results/stats/transactions_number_',num2str(CAP),'CAP.csv']);
@@ -26,11 +30,10 @@ function [better_than_random,model] = grad_desc_comparison(RESTRICTED,ALGORITHM,
     % the original number of transactions minus the restriction (training)
     transactions = transactions_number - RESTRICTED;
     
+    % CALCULATE THE RANDOM MODEL
     % the random MLE value is calculated based on 
     % the number of actions and the number of transactions
     random_MLEs = - transactions * log(1 / N_ACTIONS);
-
-    close all;
 
     % LOAD THE GRAD DESC MODELs TO TEST
     % RL MODEL
@@ -49,14 +52,14 @@ function [better_than_random,model] = grad_desc_comparison(RESTRICTED,ALGORITHM,
     
     %% NOGAMMA V RANDOM
     % test the simpler model (nogamma) against the random model
+    
     % likelihood ratio test manual
     % chi_value_random = 2 * (-noGamma_MLEs - (-random_MLEs));
     % p_rand_MLE = 1-chi2cdf(chi_value_random, ngm_rnd_dof);
 
     % likelihood ratio test matlab
-    [lrt_comparison,pValue,stat,cValue] = lratiotest(random_MLEs,noGamma_MLEs, ngm_rnd_dof);
+    [comparison,pValue,stat,cValue] = lratiotest(-noGamma_MLEs,-random_MLEs, ngm_rnd_dof);
     
-    comparison = lrt_comparison;
     ng_better_than_random = noGamma.*(repmat(comparison,1,7));
     ng_better_than_random(all(ng_better_than_random==0,2),:) = [];
 
@@ -77,7 +80,7 @@ function [better_than_random,model] = grad_desc_comparison(RESTRICTED,ALGORITHM,
 %     set(gca,'FontSize',FONT_SIZE);
     
     %% MLE comparison paper figure v RANDOM
-    
+    % fig 1_1 shows the MLE values
     fig_data = [noGamma_MLEs, random_MLEs, comparison > 0, model(:,1)];
     fig_data = sortrows(fig_data,1);
     significant_players = find(fig_data(:,3) > 0);
@@ -107,14 +110,42 @@ function [better_than_random,model] = grad_desc_comparison(RESTRICTED,ALGORITHM,
         print(fig1_1, '-dpng', '-loose', fileName); 
     end
     
-
+    % fig 1_2 shows the LRT for NOGAMMA V RANDOM comparison
+    fig_data_diff = [2*(random_MLEs - noGamma_MLEs), comparison > 0 , model(:,1)];
+    fig_data_diff = sortrows(fig_data_diff,1);
+    significant_players = find(fig_data_diff(:,2) > 0);
+    
+    fig1_2 = figure();
+    hold on;
+    bar(fig_data_diff(:,1),'FaceColor',[0.7,0.7,0.7]);                                      % MLE difference as grey bars
+    plot([0 playersAmount+1],[threshold_2dof threshold_2dof],'r','LineWidth',THRESHOLD_SIZE);    % 2 dof chi-squared threshold as red line
+    plot(significant_players , max(fig_data_diff(:,1)) + max(fig_data_diff(:,1))/20,'k*','MarkerSize',MARKER_SIZE);
+    
+    xlabel('Players','FontSize',FONT_SIZE);
+    ylabel('Likelihood Ratio Test','FontSize',FONT_SIZE);
+    axis([0, 47, min(fig_data_diff(:,1)), max(fig_data_diff(:,1)) + max(fig_data_diff(:,1))/10])
+    set(gca,'FontSize',FONT_SIZE);
+    title('NoGamma model vs Random model - Likelihood Ratio Test');
+    set(gca,'XTick',1:1:46,'XTickLabel',fig_data_diff(:,3));
+    legend('LRT statistic','Significance Threshold','Significant Player','Location','NorthWest')
+    grid
+    
+    if SAVE_FIG 
+        set(gcf, 'PaperUnits', 'centimeters');
+        set(gcf, 'PaperPosition', [0 0 PRINT_WIDTH PRINT_HEIGHT]);
+        path = '../graphs/paper/LRT/';
+        fileName = [path, 'NoGamma_vs_Random_',num2str(RESTRICTED),'restricted'];
+        fileName = [fileName,'_',ALGORITHM];
+        fileName = [fileName,'_CAP',num2str(CAP),'_nAct',num2str(N_ACTIONS),'.png'];
+        print(fig1_2, '-dpng', '-loose', fileName); 
+    end
+    
     %% RL MODEL V NOGAMMA
     % test the more complex model (RL) against the simpler model (nogamma) 
     
     % likelihood ratio test matlab
-    [lrt_comparison,pValue,stat,cValue] = lratiotest(noGamma_MLEs,model_MLEs, rlm_ngm_dof);
+    [comparison,pValue,stat,cValue] = lratiotest(-model_MLEs,-noGamma_MLEs, rlm_ngm_dof);
     
-    comparison = lrt_comparison;
     fig_data = [model_MLEs, noGamma_MLEs, comparison > 0, model(:,1)];
     fig_data = sortrows(fig_data,1);
     significant_players = find(fig_data(:,3) > 0);
@@ -143,13 +174,42 @@ function [better_than_random,model] = grad_desc_comparison(RESTRICTED,ALGORITHM,
         fileName = [fileName,'_CAP',num2str(CAP),'_nAct',num2str(N_ACTIONS),'.png'];
         print(fig2_1, '-dpng', '-loose', fileName); 
     end
+    
+    % fig 2_2 shows the LRT for RL MODEL V NOGAMMA comparison
+    fig_data_diff = [2*(noGamma_MLEs - model_MLEs), comparison > 0 , model(:,1)];
+    fig_data_diff = sortrows(fig_data_diff,1);
+    significant_players = find(fig_data_diff(:,2) > 0);
+    
+    fig2_2 = figure();
+    hold on;
+    bar(fig_data_diff(:,1),'FaceColor',[0.7,0.7,0.7]);                                          % MLE difference as grey bars
+    plot([0 playersAmount+1],[threshold_1dof threshold_1dof],'r','LineWidth',THRESHOLD_SIZE);   % 1 dof chi-squared threshold as red line
+    plot(significant_players , max(fig_data_diff(:,1)) + max(fig_data_diff(:,1))/20,'k*','MarkerSize',MARKER_SIZE);
+    
+    xlabel('Players','FontSize',FONT_SIZE);
+    ylabel('Likelihood Ratio Test','FontSize',FONT_SIZE);
+    axis([0, 47, min(fig_data_diff(:,1)), max(fig_data_diff(:,1)) + max(fig_data_diff(:,1))/10])
+    set(gca,'FontSize',FONT_SIZE);
+    title('RL model vs NoGamma model - Likelihood Ratio Test');
+    set(gca,'XTick',1:1:46,'XTickLabel',fig_data_diff(:,3));
+    legend('LRT statistic','Significance Threshold','Significant Player','Location','NorthWest')
+    grid
+    
+    if SAVE_FIG 
+        set(gcf, 'PaperUnits', 'centimeters');
+        set(gcf, 'PaperPosition', [0 0 PRINT_WIDTH PRINT_HEIGHT]);
+        path = '../graphs/paper/LRT/';
+        fileName = [path, 'RL_vs_NoGamma_',num2str(RESTRICTED),'restricted'];
+        fileName = [fileName,'_',ALGORITHM];
+        fileName = [fileName,'_CAP',num2str(CAP),'_nAct',num2str(N_ACTIONS),'.png'];
+        print(fig2_2, '-dpng', '-loose', fileName); 
+    end
 
     %% RL MODEL V RANDOM
     % test the more complex model (RL) against the random model
 
-    [lrt_comparison,pValue,stat,cValue] = lratiotest(random_MLEs,model_MLEs, rlm_rnd_dof);
-
-    comparison = lrt_comparison;
+    [comparison,pValue,stat,cValue] = lratiotest(-model_MLEs,-random_MLEs, rlm_rnd_dof);
+    
     better_than_random = model.*(repmat(comparison,1,7));
     better_than_random(all(better_than_random==0,2),:) = [];
     
@@ -176,8 +236,38 @@ function [better_than_random,model] = grad_desc_comparison(RESTRICTED,ALGORITHM,
         set(gcf, 'PaperUnits', 'centimeters');
         set(gcf, 'PaperPosition', [0 0 PRINT_WIDTH PRINT_HEIGHT]);
         path = '../graphs/paper/';
-        fileName = [path, 'RL_vs_Random',num2str(RESTRICTED),'restricted'];
+        fileName = [path, 'RL_vs_Random_',num2str(RESTRICTED),'restricted'];
         fileName = [fileName,'_',ALGORITHM];
         fileName = [fileName,'_CAP',num2str(CAP),'_nAct',num2str(N_ACTIONS),'.png'];
         print(fig3_1, '-dpng', '-loose', fileName); 
+    end
+    
+    % fig 2_2 shows the LRT for RL MODEL V NOGAMMA comparison
+    fig_data_diff = [2*(random_MLEs - model_MLEs), comparison > 0 , model(:,1)];
+    fig_data_diff = sortrows(fig_data_diff,1);
+    significant_players = find(fig_data_diff(:,2) > 0);
+    
+    fig3_2 = figure();
+    hold on;
+    bar(fig_data_diff(:,1),'FaceColor',[0.7,0.7,0.7]);                                          % MLE difference as grey bars
+    plot([0 playersAmount+1],[threshold_3dof threshold_3dof],'r','LineWidth',THRESHOLD_SIZE);   % 3 dof chi-squared threshold as red line
+    plot(significant_players , max(fig_data_diff(:,1)) + max(fig_data_diff(:,1))/20,'k*','MarkerSize',MARKER_SIZE);
+    
+    xlabel('Players','FontSize',FONT_SIZE);
+    ylabel('Likelihood Ratio Test','FontSize',FONT_SIZE);
+    axis([0, 47, min(fig_data_diff(:,1)), max(fig_data_diff(:,1)) + max(fig_data_diff(:,1))/10])
+    set(gca,'FontSize',FONT_SIZE);
+    title('RL model vs Random - Likelihood Ratio Test');
+    set(gca,'XTick',1:1:46,'XTickLabel',fig_data_diff(:,3));
+    legend('LRT statistic','Significance Threshold','Significant Player','Location','NorthWest')
+    grid
+    
+    if SAVE_FIG 
+        set(gcf, 'PaperUnits', 'centimeters');
+        set(gcf, 'PaperPosition', [0 0 PRINT_WIDTH PRINT_HEIGHT]);
+        path = '../graphs/paper/LRT/';
+        fileName = [path, 'RL_vs_Random_',num2str(RESTRICTED),'restricted'];
+        fileName = [fileName,'_',ALGORITHM];
+        fileName = [fileName,'_CAP',num2str(CAP),'_nAct',num2str(N_ACTIONS),'.png'];
+        print(fig3_2, '-dpng', '-loose', fileName); 
     end
